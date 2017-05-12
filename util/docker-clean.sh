7b12acb541b29e69a781077838888dacae49689f
planetf1 <jonesn@uk.ibm.com>
2017-05-12 16:14:29 +0000

    Further changes debugging a basic setup

--- util/docker-clean.sh
+++ util/docker-clean.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
 # Will leave base ubuntu -- but get rid of the rest
-CONTID=`docker ps -a|tail -n +2| grep -vy ubuntu| awk '{print $1}'`
-IMGID=`docker ps -a|tail -n +2| grep -vy ubuntu| awk '{print $2}'`
+CONTID=`docker ps -a|tail -n +2| grep -vy ubuntu|grep -v mariadb| awk '{print $1}'`
+#IMGID=`docker ps -a|tail -n +2| grep -vy ubuntu|grep -v mariadb| awk '{print $2}'`
+IMGID=`docker images|tail -n +2| grep -vy ubuntu|grep -v mariadb| awk '{print $3}'`
 
 for cid in $CONTID
 do
   echo "Removing container $cid"
   docker rm $cid
 done
-for iid in $CONTID
+for iid in $IMGID
 do
   echo "Removing image $iid"
-  docker rmi $cid
+  docker rmi $iid
 done
 
 docker ps -a
+docker images
